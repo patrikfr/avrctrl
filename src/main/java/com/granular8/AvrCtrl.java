ad4890fa5572125d1f1b3a82bebd01a99d517297
Patrik Fredriksson <patrikfr@gmail.com>
2020-12-07 10:56:55 +0100

    Built out the picocli args structure.

--- src/main/java/com/granular8/AvrCtrl.java
+++ src/main/java/com/granular8/AvrCtrl.java
@@ -1,8 +1,8 @@
 package com.granular8;
 
 import picocli.CommandLine;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
+import picocli.CommandLine.*;
+import picocli.CommandLine.Model.CommandSpec;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,39 +11,29 @@ import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
-import java.util.concurrent.Callable;
+import java.util.Set;
+
+import static picocli.CommandLine.ScopeType.INHERIT;
 
 @Command(name = "avrctrl", description = "Control Denon AVR-Xx100 receivers")
-public class AvrCtrl implements Callable<Integer> {
-
+public class AvrCtrl {
   private static final String AVR_NETWORK_NAME = "denon-avr-x2100w.local";
   private static final int AVR_NETWORK_PORT = 23;
-  public static final int READ_TIMEOUT = 2000;
+  private static final int READ_TIMEOUT = 2000;
+  private static final Set<String> validInputSources = Set.of("MPLAY", "CD", "BD");
 
-  @CommandLine.Parameters(index = "0", description = "The action to perform.")
-  private String action;
+  @Spec
+  CommandSpec spec;
 
-  @CommandLine.Parameters(index = "1", description = "Action input.", defaultValue = "")
-  private String actionParameter;
-
-  @Option(names = {"-d", "--debug"})
+  @Option(names = {"-d", "--debug"}, scope = INHERIT)
   private boolean debug;
 
-  //avrctrl select-input MPLAY
-  //avrctrl on
-  //avrctrl standby
-  //avrctrl vol-up
-  //avrctrl vol-down
-  //avrctrl vol <int>
-  //-d print output
-  @Override
-  public Integer call() {
+  private int sendCommand(final String command) {
     try (
         final Socket echoSocket = new Socket(AVR_NETWORK_NAME, AVR_NETWORK_PORT);
         final PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-        final BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+        final BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()))
     ) {
-
       if (debug) {
         echoSocket.setSoTimeout(READ_TIMEOUT);
         final Runnable eventReader = () -> {
@@ -61,12 +51,12 @@ public class AvrCtrl implements Callable<Integer> {
         new Thread(eventReader).start();
       }
 
-      final String command = action + actionParameter + "\r";
-      out.printf(command);
+      out.printf(command + "\r");
 
       if (debug) {
         Thread.sleep(READ_TIMEOUT);
       }
+
       return 0;
     } catch (final ConnectException ce) {
       System.out.printf("Failed to connect to %s%n", AVR_NETWORK_NAME);
@@ -76,15 +66,47 @@ public class AvrCtrl implements Callable<Integer> {
     return -1;
   }
 
-  //COMMANDS:
-  //SI {MPLAY, CD, BD}
-  //PWON
-  //PWSTANDBY
-  //MV <int>
-  //MVUP
-  //MVDOWN
+  @Command(name = "select-input")
+  int selectInput(@Parameters(paramLabel = "<input source>") final String input) {
+    if (!validInputSources.contains(input)) {
+      throw new ParameterException(spec.commandLine(),
+          String.format("Invalid value '%s' for action 'select-input': " +
+              "<input source> must be one of %s.", input, String.join(",", validInputSources)));
+    }
+    return sendCommand("SI" + input);
+  }
 
-  //avrctrl select-input MPLAY
+  @Command(name = "on", description = "Turn on receiver")
+  int on() {
+    return sendCommand("PWON");
+  }
+
+  @Command(name = "standby", description = "Set receiver to standby")
+  int standby() {
+    return sendCommand("PWSTANDBY");
+  }
+
+  @Command(name = "vol-up", description = "Increase master volume 0.5 step")
+  int volUp() {
+    return sendCommand("MVUP");
+  }
+
+  @Command(name = "vol-down", description = "Decrease master volume 0.5 step")
+  int volDown() {
+    return sendCommand("MVDOWN");
+  }
+
+  @Command(name = "vol", description = "Set master volume")
+  int vol(@Parameters(paramLabel = "<master volume level>") int volume) {
+    if (volume < 0 || volume > 70) {
+      throw new ParameterException(spec.commandLine(),
+          String.format("Invalid value '%d' for action 'vol': " +
+              "<master volume level> must be between 0 and 70.", volume));
+    }
+    return sendCommand("MV" + volume);
+  }
+
+  //avrctrl select-input {MPLAY, CD}
   //avrctrl on
   //avrctrl standby
   //avrctrl vol-up
