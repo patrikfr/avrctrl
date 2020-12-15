cce47233f534009c1eff42c6da5e04c39d0396e3
Patrik Fredriksson <patrikfr@gmail.com>
2020-12-15 17:22:40 +0100

    Added support for source mappings (e.g. map 'sonos' to AVR command 'MPLAY'), overriding AVR network name, and local user config properties file (~/.avrctrl.properties).

--- src/main/java/com/granular8/AvrCtrl.java
+++ src/main/java/com/granular8/AvrCtrl.java
@@ -11,17 +11,21 @@ import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
+import static java.lang.String.CASE_INSENSITIVE_ORDER;
 import static picocli.CommandLine.ScopeType.INHERIT;
 
-@Command(name = "avrctrl", description = "Control Denon AVR-Xx100 receivers", version = "${bundle:build.version}", resourceBundle = "AvrCtrl", mixinStandardHelpOptions = true)
+@Command(name = "avrctrl", description = "Control Denon AVR-Xx100 receivers", version = "${bundle:build.version}", resourceBundle = "AvrCtrl", mixinStandardHelpOptions = true, defaultValueProvider = PropertiesDefaultProvider.class)
 public class AvrCtrl {
 
-  private static final String AVR_NETWORK_NAME = "denon-avr-x2100w.local"; //TODO: Make this configurable from cli
-  private static final int AVR_NETWORK_PORT = 23;
   private static final int READ_TIMEOUT = 2000;
-  private static final Set<String> validInputSources = Set.of("MPLAY", "CD", "BD");
+  private static final Set<String> validInputSources = Set.of(
+      "PHONO", "CD", "TUNER DVD", "BD", "TV SAT/CBL", "MPLAY", "GAME", "HDRADIO", "NET", "PANDORA",
+      "SIRIUSXM", "SPOTIFY", "LASTFM", "FLICKR", "IRADIO", "SERVER", "FAVORITES", "AUX1", "AUX2", "AUX3",
+      "AUX4", "AUX5", "AUX6", "AUX7", "BT", "USB/IPOD", "USB", "IPD", "IRP", "FVP");
 
   @Spec
   CommandSpec spec;
@@ -29,52 +33,21 @@ public class AvrCtrl {
   @Option(names = {"-d", "--debug"}, scope = INHERIT)
   private boolean debug;
 
-  private int sendCommand(final String command) {
-    try (
-        final Socket echoSocket = new Socket(AVR_NETWORK_NAME, AVR_NETWORK_PORT);
-        final PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-        final BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()))
-    ) {
-      if (debug) {
-        echoSocket.setSoTimeout(READ_TIMEOUT);
-        final Runnable eventReader = () -> {
-          try {
-            //noinspection InfiniteLoopStatement
-            while (true) {
-              System.out.println(in.readLine());
-            }
-          } catch (final SocketTimeoutException ste) {
-            //Silently suppress
-          } catch (final Exception e) {
-            e.printStackTrace(System.out);
-          }
-        };
-        new Thread(eventReader).start();
-      }
+  @Option(names = {"-n", "--host-name"}, scope = INHERIT, description = "The AVR network name or IP address (default: ${DEFAULT-VALUE})")
+  private String host = "denon-avr-x2100w.local";
 
-      out.printf(command + "\r");
-
-      if (debug) {
-        Thread.sleep(READ_TIMEOUT);
-      }
-
-      return 0;
-    } catch (final ConnectException ce) {
-      System.out.printf("Failed to connect to %s%n", AVR_NETWORK_NAME);
-    } catch (final IOException | InterruptedException e) {
-      e.printStackTrace();
-    }
-    return -1;
-  }
+  @Option(names = {"-s", "--sources"}, scope = INHERIT, split = "\\|", splitSynopsisLabel = "|", description = "Sources map in format '-s \"Sonos=CD|AppleTV=MPLAY\"'")
+  private Map<String, String> sourcesMap;
 
   @Command(name = "select-input")
   int selectInput(@Parameters(paramLabel = "<input source>") final String input) {
-    if (!validInputSources.contains(input)) {
+    final String mappedSource = mapSource(input);
+    if (!validInputSources.contains(mappedSource)) {
       throw new ParameterException(spec.commandLine(),
           String.format("Invalid value '%s' for action 'select-input': " +
-              "<input source> must be one of %s.", input, String.join(",", validInputSources)));
+              "<input source> must be mappable to one of %s. Source map: %s", input, String.join(",", validInputSources), sourcesMap));
     }
-    return sendCommand("SI" + input);
+    return sendCommand("SI" + mappedSource);
   }
 
   @Command(name = "on", description = "Turn on receiver")
@@ -107,13 +80,49 @@ public class AvrCtrl {
     return sendCommand("MV" + volume);
   }
 
-  //avrctrl select-input {MPLAY, CD}
-  //avrctrl on
-  //avrctrl standby
-  //avrctrl vol-up
-  //avrctrl vol-down
-  //avrctrl vol <int>
-  //-d print output
+  private String mapSource(final String source) {
+    final Map<String, String> sourceMapIgnoreKeyCase = new TreeMap<>(CASE_INSENSITIVE_ORDER);
+    sourceMapIgnoreKeyCase.putAll(sourcesMap);
+    return sourceMapIgnoreKeyCase.getOrDefault(source, source);
+  }
+
+  private int sendCommand(final String command) {
+    try (
+        final Socket echoSocket = new Socket(host, 23);
+        final PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+        final BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()))
+    ) {
+      if (debug) {
+        echoSocket.setSoTimeout(READ_TIMEOUT);
+        final Runnable eventReader = () -> {
+          try {
+            //noinspection InfiniteLoopStatement
+            while (true) {
+              System.out.println(in.readLine());
+            }
+          } catch (final SocketTimeoutException ste) {
+            //Silently suppress
+          } catch (final Exception e) {
+            e.printStackTrace(System.out);
+          }
+        };
+        new Thread(eventReader).start();
+      }
+
+      out.printf(command + "\r");
+
+      if (debug) {
+        Thread.sleep(READ_TIMEOUT);
+      }
+
+      return 0;
+    } catch (final ConnectException ce) {
+      System.out.printf("Failed to connect to %s%n", host);
+    } catch (final IOException | InterruptedException e) {
+      e.printStackTrace();
+    }
+    return -1;
+  }
 
   public static void main(final String[] args) {
     int exitCode = new CommandLine(new AvrCtrl()).execute(args);
